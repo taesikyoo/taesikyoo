c2fe638662fa11664973b245dcf33abc724d6bf9
taesikyoo <yrns0907@gmail.com>
2020-08-12 14:42:35 +0900

    blog 업데이트를 위해 main.py 추가

---
+++ main.py
@@ -0,0 +1,21 @@
+import feedparser, datetime
+
+tistory_blog_uri="https://isholiday.tistory.com" #Your blog address here
+feed = feedparser.parse(tistory_blog_uri+"/rss")
+
+markdown_text = """# Hello, there!
+Your introduction goes here
+## Recent blog posts
+""" # list of blog posts will be appended here
+
+lst = []
+
+
+for i in feed['entries']:
+    dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
+    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+    print(i['link'], i['title'])
+
+f = open("test.md",mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
