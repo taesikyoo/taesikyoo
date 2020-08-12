a312727604cf03c8982cf5e0fc1de4540ebfeb9e
taesikyoo <yrns0907@gmail.com>
2020-08-12 14:52:00 +0900

    main.py 수정

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import feedparser, datetime
 tistory_blog_uri="https://isholiday.tistory.com" #Your blog address here
 feed = feedparser.parse(tistory_blog_uri+"/rss")
 
+markdown_text = ""
 lst = []
 
 for i in feed['entries']:
     dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
-    markdown_text = f"- [{i['title']}]({i['link']}) - {dt}<br>\n"
+    markdown_text += f"- [{i['title']}]({i['link']}) - {dt}<br>\n"
     print(i['link'], i['title'])
 
 f = open("test.md",mode="w", encoding="utf-8")
